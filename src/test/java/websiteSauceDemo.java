f90056bbd689e3a59039d40d84d3f5d0d80e4e6d
danielgrecu <danielgrecu88@gmail.com>
2025-07-30 20:42:51 +0300

    Automatizare: Adăugarea a două produse în coș și finalizarea comenzii pe www.SauceDemo.com

--- src/test/java/websiteSauceDemo.java
+++ src/test/java/websiteSauceDemo.java
@@ -17,7 +17,7 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SauceDemo {
+public class websiteSauceDemo {
 
     WebDriver driver;
 
